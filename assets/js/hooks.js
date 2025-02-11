e55a5a4382f940ac24fc08086b8682e224fc899a
Howard D. Lince III <git@howard.lince.rocks>
2025-02-11 08:46:50 -0600

    Update Stripe payment form to use new Elements and Payment method

--- assets/js/hooks.js
+++ assets/js/hooks.js
@@ -92,19 +92,26 @@ const Hooks = {
   },
   StripeForm: {
     async mounted() {
+      const clientSecret = this.el.dataset.clientSecret;
+      let elements;
       // Load Stripe.js dynamically
       await loadScript('https://js.stripe.com/v3/');
       
       if (window.stripeConfig === undefined) {
         const stripe = Stripe(this.el.dataset.stripeKey);
-        const elements = stripe.elements();
+        elements = stripe.elements({clientSecret: this.el.dataset.clientSecret});
         const addressElement = elements.create("address", {
           mode: "billing",
           fields: {
             country: "US"
           }
         });
-        const cardElement = elements.create("card");
+        const cardElement = elements.create("payment", {
+          layout: "accordion",
+          defaultCollapsed: true,
+          radios: true,
+          spacedAccordionItems: false
+        });
         window.stripeConfig = { stripe, elements, addressElement, cardElement };
       }
 
@@ -112,27 +119,37 @@ const Hooks = {
       const form = document.getElementById("payment-form");
 
       window.stripeConfig.addressElement.mount("#address-element");
-      window.stripeConfig.cardElement.mount("#card-element");
-
-      const clientSecret = this.el.dataset.clientSecret;
+      window.stripeConfig.cardElement.mount("#payment-element");
 
       form.addEventListener("submit", async (event) => {
         event.preventDefault();
+
+        if (!window.stripeConfig.stripe) {
+          console.error("Stripe is not loaded");
+          return;
+        }
+
+          // Trigger form validation and wallet collection
+        const {error: submitError} = await elements.submit();
+        if (submitError) {
+          paymentErrors.textContent = submitError.message;
+          return;
+        }
+
         const billingDetails = await window.stripeConfig.addressElement.getValue();
 
-        const { payment, error } = await window.stripeConfig.stripe.confirmCardPayment(clientSecret, {
-          payment_method: {
-            card: window.stripeConfig.cardElement,
-            billing_details: billingDetails.value
-          }
+        const { payment, error } = await window.stripeConfig.stripe.confirmPayment({
+          elements,
+          clientSecret,
+          confirmParams: {
+            return_url: window.location.origin + "/sponsorships/success?payment_intent_id=" + this.el.dataset.paymentIntentId
+          },
         });
 
         if (error) {
           paymentErrors.textContent = error.message;
           return;
         }
-
-        window.location.href = "/sponsorships/success?payment_intent_id=" + this.el.dataset.paymentIntentId;
       });
     }
   },
